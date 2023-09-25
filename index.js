21ea6e9e2ae496ea3230372f882c3184de512ca7
PatrickBett <patrickbett018@gmail.com>
2023-09-26 00:04:09 +0300

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+//fuzzyMatc function
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+//matchName
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
